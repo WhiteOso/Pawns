d90fe1bc203be9e4afc4d3a044fad08655ff0df1
Joaquin Calero <joaquim.calero@gmail.com>
2020-04-01 20:31:03 +0200

    Add project files.

---
+++ Pawns/PawnHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace Pawns
+{
+    [Microsoft.AspNet.SignalR.Hubs.HubName("pawnHub")]
+    public class PawnHub : Hub
+    {
+        public void Send(int x, int y, int idx)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.Others.broadcastMessage(x, y, idx);
+        }
+    }
+}
